205c50441c0d0f6a73bd0e91524b6a51efbdbb00
BeboKhouja <73379481+BeboKhouja@users.noreply.github.com>
2024-08-28 18:38:19 +0700

    Test new build notifier

--- app/src/main/java/com/mokkachocolata/pcsimulatorsaveeditorandroidport/MainActivity2.kt
+++ app/src/main/java/com/mokkachocolata/pcsimulatorsaveeditorandroidport/MainActivity2.kt
@@ -52,7 +52,6 @@ import java.io.IOException
 import java.io.InputStreamReader
 import java.lang.Integer.parseInt
 import java.lang.Long.parseLong
-import java.security.Key
 import kotlin.properties.Delegates
 
 class ReadTextFromUriThread : Runnable {
